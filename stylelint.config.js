3ce198feb26c484358388188554924faa7269008
pyncz <pavelyanycz@gmail.com>
2022-01-09 05:39:13 +0300

    style: update stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  customSyntax: 'postcss-html',
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recommended-vue',
-  ],
+  extends: ['stylelint-config-standard'],
   rules: {
     'declaration-empty-line-before': 'never',
+    'rule-empty-line-before': 'never',
+    'value-list-max-empty-lines': 1,
     'no-descending-specificity': null,
     'selector-pseudo-element-no-unknown': [
       true,
@@ -13,6 +11,9 @@ module.exports = {
         ignorePseudoElements: ['v-deep'],
       },
     ],
+    'at-rule-empty-line-before': ['always', {
+      except: ['inside-block', 'blockless-after-blockless', 'first-nested'],
+    }],
     'at-rule-no-unknown': [
       true,
       {
@@ -30,6 +31,7 @@ module.exports = {
           'if',
           'else',
           'layer',
+          'screen',
         ],
       },
     ],
